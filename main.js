67776bc3f6f0c74498901c87c83e0a324472c12c
Christopher Herrington <christopher.herrington@outlook.com>
2016-08-03 22:47:31 -0400

    fixed 'new Date' scope and write to html function

--- main.js
+++ main.js
@@ -1,27 +1,38 @@
 var dateElement = document.getElementById('date');
 
-var dateString = undefined;
 
 function updateDate () {
   let date = new Date();
   let h = date.getHours()
   let m = date.getMinutes()
   let s = date.getSeconds()
+  let dateString = undefined
   if (h < 10) {h = "0" + h}
   if (m < 10) {m = "0" + m}
   if (s < 10) {s = "0" + s}
-  dateString = `${h}:${m}:${s}`;
-  drawTime();
+  dateString = `#${h}${m}${s}`;
+  return dateString;
 }
 
-var drawTime = function () {
-dateElement.innerHTML = dateString;
-}
+// function toHex (i) {
+//   let hexString = (i+0x10000).toString(16).substr(-4).toUpperCase();
+//      console.log(hexString)
+//   }
+
+document.getElementById('body').style.width = value;
+
+var writeToHTML = function (data) {
+  dateElement.innerHTML = data;
+};
+
+function dateController() {
+
+};
 
 
 function init() {
   updateDate()
-  var dateTimer = window.setInterval(updateDate, 1000);
+  var dateTimer = window.setInterval(dateController, 1000);
 };
 
 init();
