ec9fbbf810d79608ef62f826cb1939e88f91144c
Christopher Herrington <christopher.herrington@outlook.com>
2016-08-04 00:27:37 -0400

    Added Hex value parsing logic, as well as changing the controllers function

--- main.js
+++ main.js
@@ -1,38 +1,54 @@
 var dateElement = document.getElementById('date');
 
+init();
+
+function init() {
+  updateHTML("Welcome!")
+  var dateTimer = window.setInterval(dateController, 1000);
+};
 
 function updateDate () {
   let date = new Date();
   let h = date.getHours()
   let m = date.getMinutes()
   let s = date.getSeconds()
-  let dateString = undefined
   if (h < 10) {h = "0" + h}
   if (m < 10) {m = "0" + m}
   if (s < 10) {s = "0" + s}
-  dateString = `#${h}${m}${s}`;
+  let dateString = `${h}${m}${s}`
+  console.log(dateString)
   return dateString;
-}
+};
 
-// function toHex (i) {
-//   let hexString = (i+0x10000).toString(16).substr(-4).toUpperCase();
-//      console.log(hexString)
-//   }
+function updateStyle (value) {
+  document.body.style.backgroundColor = value;
+};
 
-document.getElementById('body').style.width = value;
+function updateHTML (date) {
+  dateElement.innerHTML = date;
+};
 
-var writeToHTML = function (data) {
-  dateElement.innerHTML = data;
+function toHex (i) {
+  let dateNum = parseInt(i, 10)
+  return hexString = dateNum.toString(16)
+};
+
+function hexNormalize (str) {
+  var strNorm = str
+  let i = 6 - str.length;
+  for (i; i > 0; i--) {
+     strNorm = "A" + strNorm
+     console.log("i = ", i)
+  };
+  strNorm = "#" + strNorm;
+  console.log("strNorm: ", strNorm)
+  return strNorm;
 };
 
 function dateController() {
+  let timeStr = updateDate()
+  let timeHex = hexNormalize(toHex(timeStr))
+  updateHTML(timeHex)
+  updateStyle("#" + timeStr)
 
 };
-
-
-function init() {
-  updateDate()
-  var dateTimer = window.setInterval(dateController, 1000);
-};
-
-init();
